93b3954ce50825408d9c5d5ce4cd04a1433b3948
iva980 <sulginivan@gmail.com>
2023-01-29 19:54:31 +0700

    29 января

---
+++ lesson_18/script.js
@@ -0,0 +1,5 @@
+let stroka = "Hello world!";
+let color = prompt("Какой твой любимый цвет?");
+alert(`Пользователь ответил - ${color}`);
+console.log(typeof stroka);
+console.log(`${stroka} Я январь`);
