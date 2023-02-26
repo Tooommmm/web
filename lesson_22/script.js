a6d84c040868e6bb9d6d5c01afa09dedbaec20f2
Tomyum1 <fuitorsil@gmail.com>
2023-02-26 19:54:52 +0700

    100 СЛАЙД!!!!!!!!!!!!!!!!

---
+++ lesson_22/script.js
@@ -0,0 +1,76 @@
+// for(let jump=1; jump<=10; jump++){
+//     console.log("Прыг " + jump)
+// }
+
+// console.log("Прыг 1")
+// console.log("Прыг 2")
+// console.log("Прыг 3")
+// console.log("Прыг 4")
+// console.log("Прыг 5")
+// console.log("Прыг 6")
+// console.log("Прыг 7")
+// console.log("Прыг 8")
+// console.log("Прыг 9")
+// console.log("Прыг 10") ДОЛГО ОЧИНЬ
+
+// let a = ["Привет", 7, [12, 13, 14], {
+//     firstname: "Андрей",
+//     age: 14,
+//     job: "Ученик"
+// }]
+
+// let l = []
+
+// for(let u = 0; u < a.length; u++){
+//     l.push(typeof a[u])
+// }
+
+// console.log(l)
+
+// let a = [1920, 2013, 2020, 1996, 1630]
+
+// let b = []
+
+// for(let u = 0; u < a.length; u++){
+//     b.push(2023 - a[u])
+// }
+
+// console.log(b)
+
+// const givenYears = [1920,2013,2020,1996,1630]
+
+// let Years = []
+
+// for(let u = 0; u < givenYears.length; u++){
+//     Years.unshift(givenYears[u])
+// }
+
+// console.log(Years)
+
+// for(let jump=1; jump<=10; jump++){
+//     if (jump == 5){
+//         continue
+//     }
+//     console.log("Прыг " + jump)
+//     for(let pri=1; pri<=3; pri++){
+//         if (jump == 8){
+//             break
+//         }
+//         console.log("Присед " + pri)
+//     }
+// }
+
+// let x = 0
+// while (x < 10){
+//     x++;
+//     console.log("Прыг " + x)
+// }
+
+// while (true){
+//     let a = prompt("дай норм число")
+//     if(a<10){
+//         break
+//     }
+// }
+
+// 100 СЛАЙД!!!!!!!!!!!!!!!!!!!
